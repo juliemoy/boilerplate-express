80f2c5700622c24974a4689ef640e0a243137a38
juliemoy <juliemoy.jm@gmail.com>
2021-09-29 12:37:01 -0400

    added routing

--- myApp.js
+++ myApp.js
@@ -4,12 +4,23 @@ var app = express();
 
 console.log("Hello World");
 
+app.use(express.static(__dirname + "/public"));
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-    res.send('Hello Express');
+    var absolutePath = __dirname + "/views/index.html";
+    res.sendFile(absolutePath);
 })
 
-
+app.get("/json", function(req,res){
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  }else {
+    res.json({"message": "Hello json"});  
+  }
+  
+})
 
 
 
